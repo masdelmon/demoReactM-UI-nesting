894f6bdaa110a9223228c4c762630d7d93f77285
Massimiliano <masdelmon@hotmail.it>
2019-02-11 16:49:33 +0100

    Update stepper.js

--- src/stepper.js
+++ src/stepper.js
@@ -32,11 +32,11 @@ function getSteps() {
 function getStepContent(step) {
   switch (step) {
     case 0:
-      return '<input type=""text"" placeholder=""username""/>';
+      return '<input/>';
     case 1:
       return "An ad group contains one or more ads which target a shared set of keywords.";
     case 2:
-      return '<button type=""submit"">Invio</button>';
+      return '<button>Invio</button>';
     default:
       return "Unknown step";
   }
