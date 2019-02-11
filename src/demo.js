3c356d9a376589a1a031a1f69cd7c563fee7f405
Massimiliano <masdelmon@hotmail.it>
2019-02-11 22:26:24 +0100

    Update demo.js

--- src/demo.js
+++ src/demo.js
@@ -7,6 +7,7 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import VerticalLinearStepper from "./stepper.js";
+import Login from "./login.js";
 
 function TabContainer({ children, dir }) {
   return (
