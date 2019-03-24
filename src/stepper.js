d147230944da425967afeb0cb37b34e681db6958
Massimiliano <masdelmon@hotmail.it>
2019-03-24 22:03:05 +0100

    Update stepper.js

--- src/stepper.js
+++ src/stepper.js
@@ -1,1 +1,134 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import StepContent from "@material-ui/core/StepContent";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
+const styles = theme => ({
+  root: {
+    width: "90%"
+  },
+  button: {
+    marginTop: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+  actionsContainer: {
+    marginBottom: theme.spacing.unit * 2
+  },
+  resetContainer: {
+    padding: theme.spacing.unit * 3
+  }
+});
+
+function getSteps() {
+  return ["Select campaign settings", "Create an ad group", "Create an ad"];
+}
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return '<div className = \"form-group\"><input type=\"text\" className = \"form-control\" placeholder=\"username\" /></div>';
+    case 1:
+      return "An ad group contains one or more ads which target a shared set of keywords.";
+    case 2:
+      return '<button type=\"submit\" className = \"\" onClick={}>Invio</button>';
+    default:
+      return "Unknown step";
+  }
+}
+
+class VerticalLinearStepper extends React.Component {
+  state = {
+    activeStep: 0
+  };
+
+  handleNext = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep + 1
+    }));
+  };
+
+  handleBack = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep - 1
+    }));
+  };
+
+  handleReset = () => {
+    this.setState({
+      activeStep: 0
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const steps = getSteps();
+    const { activeStep } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <div style={{marginTop:"100px", minHeight:"70vh"}}>
+            <div className = "container">
+                <div className = "row">
+                    <div className = "col-6 mr-auto ml-auto">
+                       <form onSubmit={this.onSubmit.bind(this)}>
+                         
+        <Stepper activeStep={activeStep} orientation="vertical">
+          {steps.map((label, index) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+              <StepContent>
+                <Typography>{getStepContent(index)}</Typography>
+                <div className={classes.actionsContainer}>
+                  <div>
+                    <Button
+                      disabled={activeStep === 0}
+                      onClick={this.handleBack}
+                      className={classes.button}
+                    >
+                      Back
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={this.handleNext}
+                      className={classes.button}
+                    >
+                      {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                    </Button>
+                  </div>
+                </div>
+              </StepContent>
+            </Step>
+          ))}
+        </Stepper>
+         </form>
+                
+
+        {activeStep === steps.length && (
+          <Paper square elevation={0} className={classes.resetContainer}>
+            <Typography>All steps completed - you&apos;re finished</Typography>
+            <Button onClick={this.handleReset} className={classes.button}>
+              Reset
+            </Button>
+          </Paper>
+        )}
+            </div>
+              </div>
+         </div>
+     </div>
+      </div>
+    );
+  }
+}
+
+VerticalLinearStepper.propTypes = {
+  classes: PropTypes.object
+};
+
+export default withStyles(styles)(VerticalLinearStepper);
