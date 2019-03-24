b0a82415d69b3f93806d0ca790fa4111e6befe88
Massimiliano <masdelmon@hotmail.it>
2019-03-24 22:48:26 +0100

    Create stepper.js

---
+++ src/stepper.js
@@ -0,0 +1,139 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import StepContent from "@material-ui/core/StepContent";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+const styles = theme => ({
+  root: {
+    width: "100%"
+  },
+  button: {
+    marginTop: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+  actionsContainer: {
+    marginBottom: theme.spacing.unit * 2
+  },
+  resetContainer: {
+    padding: theme.spacing.unit * 3
+  }
+});
+
+
+function getSteps() {
+  return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+}
+
+
+
+
+class VerticalLinearStepper extends React.Component {
+  constructor(props){
+      super(props);
+      this.state = {
+         username: '',
+         password: '',
+         activeStep:0
+      };
+  }
+
+  getStepContent(step) {
+  switch (step) {
+    case 0:
+      return (<input type="text" />);
+    case 1:
+      return (<input type="text" />);
+    case 2:
+      return (<button>Invio</button>);
+    default:
+      return ('invalid option');
+  }
+}
+       
+      
+  handleNext = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep + 1
+    }));
+  };
+
+  handleBack = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep - 1
+    }));
+  };
+
+  handleReset = () => {
+    this.setState({
+      activeStep: 0
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const steps = getSteps();
+    const { activeStep } = this.state;
+
+    return (
+      <div className={classes.root}>
+        
+                       <form>
+                         
+        <Stepper activeStep={activeStep} orientation="vertical">
+          {steps.map((label, index) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+              <StepContent>
+                {getStepContent(index)}
+                <div className={classes.actionsContainer}>
+                  <div>
+                    <Button
+                      disabled={activeStep === 0}
+                      onClick={this.handleBack}
+                      className={classes.button}
+                    >
+                      Back
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={this.handleNext}
+                      className={classes.button}
+                    >
+                      {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                    </Button>
+                  </div>
+                </div>
+              </StepContent>
+            </Step>
+          ))}
+        </Stepper>
+         </form>
+                
+
+        {activeStep === steps.length && (
+          <Paper square elevation={0} className={classes.resetContainer}>
+            <Typography>All steps completed - you&apos;re finished</Typography>
+            <Button onClick={this.handleReset} className={classes.button}>
+              Reset
+            </Button>
+          </Paper>
+        )}
+     
+     </div>
+ 
+    );
+  }
+}
+
+VerticalLinearStepper.propTypes = {
+  classes: PropTypes.object
+};
+
+export default withStyles(styles)(VerticalLinearStepper);
