78dc1b1ed61bff0581ac1cde98d248f2fff447e6
Massimiliano <masdelmon@hotmail.it>
2019-03-31 16:47:20 +0200

    Update stepper.js

--- src/stepper.js
+++ src/stepper.js
@@ -40,7 +40,7 @@ function getStepContent(step, datafrm, updateStateUsername, updateStatePassword,
     case 2: 
      return <fieldset><legend>Address data:</legend>Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:<input id="email" name="email"  type="text" value = {datafrm.email} onChange = {updateStateEmail} />Phone&nbsp;&nbsp;&nbsp;:<input id="phone" name="phone"  type="text" value = {datafrm.phone} onChange = {updateStatePhone} /></fieldset>;
     case 3:
-     return <fieldset>Username:{datafrm.username} - Password:{datafrm.password} - <Button value="0" variant="contained" color="primary" onClick={updateStep0} className={classes.button}>Modifica</Button>Name:{datafrm.firstName} - Surname:{datafrm.lastName} - <Button value="1" variant="contained" color="primary" onClick={updateStep1} className={classes.button}>Modifica</Button>Email:{datafrm.email} - Password:{datafrm.phone} - <Button value="2" variant="contained" color="primary" onClick={updateStep2} className={classes.button}>Modifica</Button></fieldset>;
+     return <fieldset>Username:{datafrm.username} - Password:{datafrm.password} - <Button variant="contained" color="primary" onClick={updateStep0} className={classes.button}>Modifica</Button>Name:{datafrm.firstName} - Surname:{datafrm.lastName} - <Button variant="contained" color="primary" onClick={updateStep1} className={classes.button}>Modifica</Button>Email:{datafrm.email} - Password:{datafrm.phone} - <Button variant="contained" color="primary" onClick={updateStep2} className={classes.button}>Modifica</Button></fieldset>;
     default:
       return ('invalid option');
   }
@@ -167,7 +167,7 @@ class VerticalLinearStepper extends React.Component {
             <Step key={label}>
               <StepLabel>{label}</StepLabel>
               <StepContent>
-                {getStepContent(index, this.state, this.updateStateUsername, this.updateStatePassword, this.updateStateFirstName, this.updateStateLastName, this.updateStateEmail, this.updateStatePhone, this.updateStep0, this.updateStep1, this.updateStep2, this.updateStep3, classes)}
+                {getStepContent(index, this.state, this.updateStateUsername, this.updateStatePassword, this.updateStateFirstName, this.updateStateLastName, this.updateStateEmail, this.updateStatePhone, this.updateStep0, this.updateStep1, this.updateStep2, classes)}
                 <div className={classes.actionsContainer}>
                   <div>
                     <Button
